a43e7c6650951b9cab8e5ec4aa3f0ce81aee5cac
Ethan Fung <EthanEFung@gmail.com>
2017-06-10 14:49:48 -0700

    Develop test suite
    Working towards a test suite that would
    demonstrate our function works similarly
    to JSON.stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,27 +4,43 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  let JSONobj = ''
+  let JSONobj = '';
 
-  if(Array.isArray(obj)) {
-    obj.forEach(function(item) {
-      if(typeof item === 'object' {
-        stringifyJSON(item);
-      } else {
-        
+  const dataTypes = []
+  if(obj === undefined || typeof obj === 'function') {
+    return null;
+  }else if(Array.isArray(obj)) {
+    JSONobj += '[';
+    
+    obj.forEach(function(item, i) {
+      JSONobj += stringifyJSON(item);
+      if (i !== obj.length - 1) {
+        JSONobj += ',';
       }
+    }); 
+    
+    JSONobj += ']';
 
-    });
-  } else if (typeof obj === 'obj') {
-
-  } else if (typeof obj === 'number') {
+  } else if (typeof obj === 'object' && obj !== null) {
 
   } else if (typeof obj === 'string') {
-    
+    JSONobj += '"' + obj + '"'
+  } else if (typeof obj === 'boolean' || obj === null || typeof obj === 'number') {
+    JSONobj += String(obj);
   }
   return JSONobj;
   // your code goes here
 };
+
+console.log(stringifyJSON(null) === JSON.stringify(null), 'should work for null');
+console.log(stringifyJSON(true) === JSON.stringify(true), 'should work for true');
+console.log(stringifyJSON(false) === JSON.stringify(false), 'should work for false');
+console.log(stringifyJSON(1) === JSON.stringify(1), 'should work for numbers');
+console.log(stringifyJSON('hello') === JSON.stringify('hello'), 'should work for strings');
+console.log(stringifyJSON(undefined) === JSON.stringify(undefined), 'should not return for undefined');
+console.log(stringifyJSON(function(){}) === JSON.stringify(function(){}), 'should not return values that are functions');
+console.log(stringifyJSON([1, true]) === JSON.stringify([1, true]), 'should support arrays');
+console.log(stringifyJSON([null, undefined]) === JSON.stringify([null, undefined]), 'should support arrays with undefined');
 
 /*
   JSON.stringify should
@@ -47,20 +63,20 @@ var stringifyJSON = function(obj) {
     (finally) turn the entirety of the object or array into a string.
 */
 
-const example = {
-	hello: 'world!',
-	fun: true, //what happens now?
-	agreeable: undefined,
-	adder: function(a, b) {return a + b},
-	heart: 2,
-	love :['tacos', 'dogs', 3],
-	object: {adele: 'rocks'}
-}
+// const example = {
+// 	hello: 'world!',
+// 	fun: true, //what happens now?
+// 	agreeable: undefined,
+// 	adder: function(a, b) {return a + b},
+// 	heart: 2,
+// 	love :['tacos', 'dogs', 3],
+// 	object: {adele: 'rocks'}
+// }
 
-const example2 = ['hello', 2, false, undefined, {}, function(a, b) {return a + b}, ['hello', true]];
+// const example2 = ['hello', 2, false, undefined, {}, function(a, b) {return a + b}, ['hello', true]];
 
 
-console.log(JSON.stringify(example));
-console.log(JSON.stringify(example2));
-console.log(JSON.stringify(true));
-console.log(JSON.stringify(2));
+// console.log(JSON.stringify(example));
+// console.log(JSON.stringify(example2));
+// console.log(JSON.stringify(true));
+// console.log(JSON.stringify(2));
