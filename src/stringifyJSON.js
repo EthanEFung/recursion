853ea6c70b1948690ca8298eecf23d7006a2f58b
Ethan Fung <EthanEFung@gmail.com>
2017-06-12 16:26:29 -0700

    Solution #2
    commented out current solution in order
    to rethink the solution, and hopefully
    come to a better understanding of the
    function itself.
    
    Will review the MDN documentation,
    followed by a new pseudocode outline.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,67 +1,20 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
+const type = {
 
-// but you don't so you're going to write it from scratch:
+  isStr : (data) => typeof data === 'string',
+  isNull : (data) => data === null,
+  isUndef : (data) => data === undefined,
+  isFunc : (data) => typeof data === 'function',
+  isObj : (data) => typeof data === 'object',
+  isBool : (data) => typeof data === 'boolean',
+  isNum : (data) => typeof data === 'number',
+  isArr : (data) => Array.isArray(data)
 
-
-
-
-const isNull = (data) => data === null;
-const isUndef = (data) => data === undefined;
-const isFunc = (data) => typeof data === 'function';
-const isObj = (data) => typeof data === 'object';
-const isBool = (data) => typeof data === 'boolean';
-const isNum = (data) => typeof data === 'number';
-const isArr = (data) => Array.isArray(data);
-
-const isStr = (data) => typeof data === 'string';
-const nonStringables = [isUndef, isFunc];
-const stringables = [isBool, isNull, isNum];
-const byRefs = [isArr, isObj];
-
+}
 
 
 const stringifyJSON = function(obj) {
-  let JSONobj = '';
 
-  function renderItems(iterator) {
-  iterator.forEach((item, i) => {
-    JSONobj += stringifyJSON(item);
-      if (i !== iterator.length - 1) {
-        JSONobj += ',';
-      }
-    }); 
-  }  
-
-  
-  
-  if(nonStringableDataTypes.some(function(objType) {
-    return 
-  })) {
-    JSONobj = undefined;
-  } else if (dataType === 2) {
-    JSONobj += '"' + obj + '"'
-  } else if (dataType < 6) {
-    JSONobj += String(obj);
-
-  } else if(isArr(obj)) {
-    JSONobj += '[';
-    renderItems(obj);
-    JSONobj += ']';
-
-  } else if (isObj(obj)) {
-    const properties = Object.keys(obj).map(function(key){
-      return  key + ':' + obj[key];
-    });
-
-    JSONobj += '{';
-    renderItems(properties);
-    JSONobj += '}';
-  } 
-  return JSONobj;
-  // your code goes here
 };
-console.log(stringifyJSON(2))
 
 
 //THE TEST-SUITE
@@ -75,26 +28,28 @@ function compare(data) {
   console.log('  this is the result of JSON.stringify() : ', JSON.stringify(data));
 }
 
-// //primitive value support
-// test(null, 'null');
-// test(true, 'true');
-// test(false, 'false');
-// test(1, 'numbers');
-// test(1.2, 'decimal numbers');
-// test('hello', 'strings');
+//primitive value support
+test({}, 'objects');
+test(true, 'true');
+test('hello', 'strings');
+test(null, 'null');
+
+test(1, 'numbers');
+test(1.2, 'decimal numbers');
+
 
 //undefined values support
 // test(undefined, 'undefined')
 // test(function(){}, 'functions');
 
 
-//array support
-test(['hello', 'world'], 'arrays with strings');
-test([1,2, 1.2, 0, -1, 0.0000056], 'arrays with numbers');
-test([false, true], 'arrays with boolean values');
+// //array support
+// test(['hello', 'world'], 'arrays with strings');
+// test([1,2, 1.2, 0, -1, 0.0000056], 'arrays with numbers');
+// test([false, true], 'arrays with boolean values');
 
-test([null, undefined], 'arrays with null and undefined values');
-compare([null, undefined]);
+// test([null, undefined], 'arrays with null and undefined values');
+// compare([null, undefined]);
 
 
 
@@ -108,23 +63,54 @@ test({d: true, e: false}, 'objects with booleans');
 test({f: undefined, g: null}), 'objects with undefined or null values'
 test({ h: function(){} }, 'functions(should not render)');
 
+
 /*
-  JSON.stringify should
-    (1)receive any JSON accepted items (the function only works upon the collection
-       in the first argument slot of the parameters).
+  let JSONobj = '';
 
-       JSON accepted items:
-       (a) arrays
-       (b) object literals
-       (c) value : string, number, object, array, true, false, or null
-       (d) string --> develop this more
-       (e) number --> develop this more http://json.org/
-    (2)  if the object has properties it will iterate over each key and check for values
-    (3)  for each property it will turn the property into a string
+  const dataIsNotStringable = [type.isUndef, type.isFunc].some((test) => test(obj));
+  const dataIsStringable = [type.isBool, type.isNull, type.isNum].some((test) => test(obj));
 
-    (4) if the value of the property is an array, or object, run the function
-      again on the elements of that collection, and return the result as a string
+  function renderItems(collection) {
+    collection.forEach((item, i) => {
+      JSONobj += stringifyJSON(item);
+
+      let itemIsntLast = (i !== collection.length -1);
+      if (itemIsntLast) {
+        JSONobj += ',';
+      }
+    }); 
+  }  
 
 
-    (finally) turn the entirety of the object or array into a string.
+  
+  if(type.isStr(obj)) {
+    JSONobj += '"' + obj + '"';
+  } else if(dataIsNotStringable) {
+    JSONobj = undefined;
+  } else if (dataIsStringable) {
+    JSONobj += obj;
+  
+
+
+  } else if(type.isArr(obj)) {
+    JSONobj += '[';
+      renderItems(obj);
+    JSONobj += ']';
+  } else if (type.isObj(obj)) {
+
+    const properties = Object.keys(obj).map(function (key) {  
+      if(type.isStr(obj[key])) {
+        obj[key] + obj[key];
+      }
+      return ['"' + key + '":"' +  obj[key] + '"']
+    });
+    
+    
+    JSONobj += '{';
+    JSONobj += properties.join(',');
+    JSONobj += '}';
+  } 
+  return JSONobj;
+  // your code goes here
+
 */
