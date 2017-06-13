777e127ad9b7e65dabd862a32b27b9803b5726c0
Ethan Fung <EthanEFung@gmail.com>
2017-06-13 11:21:10 -0700

    Instantiate Tester Suite
    Began by utilizing the Specrunner
    HTML DOM to test several cases.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,5 +7,38 @@
 
 // You should use document.body, element.childNodes, and element.classList
 var getElementsByClassName = function(className) {
-  // your code here
+  return document.body;
 };
+
+
+//TEST SUITE
+
+function test(className, testName) {
+  let test = document.getElementsByClassName(className) === getElementsByClassName(className);
+
+  console.log(test, testName);
+  if (test === false) {
+  	console.log('  result of actual func: ', document.getElementsByClassName(className));
+  	console.log('  result of written func: ', getElementsByClassName(className));
+  }
+}
+
+test('targetClassName', 'should work for classes of multiple DOM nodes');
+test('progress', 'should work for items in a list');
+test('suite', 'should work for items in a list');
+test('test fail', 'should work for nested items');
+/*
+
+  The actual function renders an HTMLCollection, which is a collection of the 
+    elements with the given class name.  This Collection is live, and changes
+    with the deletion, or instantiation of elements with the same name.
+
+    HTMLCollection(s) have a property of length, and has a method called .item()
+    which can be called on each of the items in the collection given a 0 based
+    index.  Or can used namedItem() for non-numeric based named attributes.
+
+
+
+  Can utilze Array methods
+  should check the childNodes of each node in the DOM
+*/
