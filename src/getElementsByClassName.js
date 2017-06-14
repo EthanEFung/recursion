1fb26ab8ce618ab07d38f4a182bd5859fc232943
Ethan Fung <EthanEFung@gmail.com>
2017-06-14 15:49:45 -0700

    Render functional getElementsByClassName
    Pass the mocha tests for the function, and
    corrected the tests I had created to check
    for mistakes.  Forgot that data by
    reference renders false when compared to
    other data since they point to data that
    lies within the memory and not to
    primitive types.
    
    Now to refactor for empathy

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,56 +6,78 @@
 // But instead we're going to implement it from scratch:
 
 // You should use document.body, element.childNodes, and element.classList
+function nodeHasClass(node, className) {
+  let classes = node.classList;
+  let hasClass = false;
 
+  if(classes !== undefined) { 
+    let values = classes.values()
+    for(let val of values) {
+      if (val === className) {
+        hasClass = true;
+      }
+    }
+  } 
+  return hasClass;
+}
+
+function checkCurrNodeForClass(node, className, collection) {
+  
+  let classes = node.classList;
+  
+  if(nodeHasClass(node, className)) {
+    collection.push(node);
+  } 
+
+  if(node.hasChildNodes()) {
+    //recursive case
+    
+    for(let child of node.childNodes.values()) {
+      checkCurrNodeForClass(child, className, collection);
+    }
+
+  } else {
+    //base case
+    return collection;
+  } 
+  //check the class of the current node, and push the nodes that have the classList
+  //if the node has children, run the function for the children
+  //if the node has no more children, return the collection.
+}
 
 
 function getElementsByClassName(className) {
-  let elements = [];
 
-  function checkNode(className, node) {
-    console.log(node);  
-    
-    if(node !== null) {
-      
-      let classes = node.classList;
-      let children = node.childNodes;
-      console.log(node, children, classes)
-      //first check to see if the current node has the required class
-      for(let i = 0; i < classes.length; i++) {
-          elements.push(node);
-      }
-      
-      console.log(children.length)
-
-      //then check to see if there are any child nodes
-      for(let i = 0; i < children.length; i++){
-
-        checkNode(className, children[i])
-      } 
-    }
-  }
-  checkNode(className, document.body);
-  return elements
+  let elements = [];   
+  elements.concat(checkCurrNodeForClass(document.body, className, elements));
+  return elements;
 };
+
+
 
 //TEST SUITE
 
-const tag = (tag) => document.getElementsByTagName(tag);    
-const doc = (className) => document.getElementsByClassName(className);
-const func = (className) => getElementsByClassName(className);
-
 function test(className, testName) {
 
-  let test = doc(className) === func(className);
+  let doc = (className) => document.getElementsByClassName(className);
+  let func = (className) => getElementsByClassName(className);
+  
+  let test = func(className).every((val, i) => val === doc(className).item(i));
+  
   console.log(test, testName);
 
   if (test === false) {
-    console.log('  result of actual func: ', doc(className));
-    console.log('  result of written func: ', func(className));
+  console.log('  result of actual func: ', document.getElementsByClassName(className));
+  console.log('  result of written func: ', getElementsByClassName(className));
   }
 }
 
-test('targetClassName', 'should work for classes of multiple DOM nodes');
-test('progress', 'should work for items in a list');
-test('suite', 'should work for items in a list');
-test('test fail', 'should work for nested items');
+$(document).ready( function(){
+  test("targetClassName", 'className "targetClassName"');
+  test('progress', 'className is "progress"');
+  test('suite', 'className is "suite"');
+  test('test fail', 'className is "test" and "fail"');
+});
+
+
+
